48de737b7065a6d3dde43741fc457267b6049b7a
Megan Ratto <meganratto@Megans-MacBook-Air.local>
2022-01-12 15:08:22 -0800

    Add Feeding and Care model

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.fields import CharField
 from django.urls import reverse
 
 # Create your models here.
@@ -13,6 +14,12 @@ from django.urls import reverse
 #   Plant('Fiddle Leaf Fig', 'Ficus lyrata')
 # ]
 
+TOXICITY = (
+  ('Severe', 'Severly toxic'),
+  ('Mild', 'Mildy toxic'),
+  ('None', 'No or low toxicity')
+)
+
 class Plant(models.Model):
   name = models.CharField(max_length=200)
   latin_name = models.CharField(max_length=200)
@@ -23,3 +30,32 @@ class Plant(models.Model):
 
   def get_absolute_url(self):
     return reverse('plants_detail', kwargs={'plant_id': self.id})
+
+
+class Feeding(models.Model):
+  date = models.DateField('Feeding Date')
+  type = CharField(max_length=200)
+  instructions = models.TextField(max_length=400)
+
+  plant = models.ForeignKey(Plant, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f'{self.type} on {self.date}'
+
+
+class Care(models.Model):
+  plant = models.OneToOneField(
+    Plant, 
+    on_delete=models.CASCADE,
+    primary_key=True,
+  )
+  light = models.TextField(max_length=300)
+  water = models.TextField(max_length=300)
+  toxicity = models.CharField(
+    max_length=6,
+    choices=TOXICITY,
+    default=TOXICITY[0][0]
+  )
+
+  def __str__(self):
+    return f'{self.name} is {self.get_toxicity_display()}'
