c3ee214208481daa919aba8e55359022739e6828
Megan Ratto <meganratto@Megans-MacBook-Air.local>
2022-01-11 16:57:02 -0800

    Add Home, About, and Base pages. Confirm CSS file is connected

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  
+  path('', views.home, name='home'),
+
+  # 'about/' - About Route
+  path('about/', views.about, name='about')
 ]
